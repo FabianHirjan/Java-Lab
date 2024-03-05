cd5a3be8c7555fc85a6ab4bad49c92b11c77937f
Hirjan Fabian <35564006+FabianHirjan@users.noreply.github.com>
2024-03-05 10:30:59 +0200

    Renamed them

--- IPHW/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Ionescu myIonescu = new Ionescu(new Masina("Toyota","Neagra", 120));
-        myIonescu.ShowCars();
-
-        System.out.println(myIonescu.getNume());
-    }
-}
